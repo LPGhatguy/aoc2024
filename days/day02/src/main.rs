6797c65f2a05b839c0d77f9f9376557f7d6ffa23
Lucien Greathouse <me@lpghatguy.com>
2024-12-01 22:45:33 -0700

    Day 2 part two -- that ain't pretty

--- days/day02/src/main.rs
+++ days/day02/src/main.rs
@@ -44,6 +44,44 @@ fn part1() {
     println!("Part one: {total_safe}");
 }
 
+fn part2() {
+    let levels = parse();
+
+    let mut total_safe = 0;
+
+    for level in levels {
+        for remove in -1..(level.len() as i32) {
+            let mut level = level.clone();
+            if remove >= 0 {
+                level.remove(remove as usize);
+            }
+
+            let a = level[0];
+            let b = level[1];
+            let mode = a.cmp(&b);
+            let mut safe = true;
+
+            for slice in level.windows(2) {
+                let &[a, b] = slice else { unreachable!() };
+
+                let good = a.cmp(&b) == mode && (a - b).abs() <= 3;
+                if !good {
+                    safe = false;
+                    break;
+                }
+            }
+
+            if safe {
+                total_safe += 1;
+                break;
+            }
+        }
+    }
+
+    println!("Part two: {total_safe}");
+}
+
 fn main() {
     part1();
+    part2();
 }
